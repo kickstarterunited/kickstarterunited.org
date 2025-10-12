53eb5c2e90cc32b704ffbde64d0f1872508f2c51
JawsomeJason <thejase@gmail.com>
2025-10-12 10:39:34 -0400

    Add support for <button> `command` + `commandfor` attributes

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,28 @@
 declare namespace astroHTML.JSX {
   interface ButtonHTMLAttributes {
-    command?: string;
+    /**
+     * Specifies the action to be performed on an element being controlled by a
+     * control <button> specified via the commandfor attribute. Part of the
+     * Invoker Commands API. [More
+     * info](https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/button#command)
+     */
+    command?:
+      | "show-modal"
+      | "close"
+      | "request-close"
+      | "show-popover"
+      | "hide-popover"
+      | "toggle-popover"
+      // See: https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/button#custom_values
+      | "--your-custom-value"
+      // Allow any other string for forward compatibility
+      | (string & {});
+    /**
+     * Turns a <button> element into a button, controlling the given interactive
+     * element; takes the ID of the element to control as its value. Part of the
+     * Invoker Commands API. [More
+     * info](https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/button#commandfor)
+     */
     commandfor?: string;
   }
 }
