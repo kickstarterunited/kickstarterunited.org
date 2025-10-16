bca3ae1ac923f4c614851d4e051044411ebe72e9
Aidan <a.lavis@kickstarter.com>
2025-10-15 21:47:53 -0400

    load from google sheets

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,24 +1,53 @@
+import { useEffect, useState } from "preact/hooks";
+import LoadingSpinner from "./LoadingSpinner";
+
+const SHEET_URL =
+  "https://docs.google.com/spreadsheets/d/1pMPA8hdCaXvvy9hJyAp1zEi7y7_QVZMYZy1CEPsAilc";
+const SHEET_EXPORT_URL = `${SHEET_URL}/export?format=csv&gid=0`;
+
+async function fetchFromSheets() {
+  const response = await fetch(SHEET_EXPORT_URL);
+  const csv = await response.text();
+  const rows = csv.split("\n").map((row) => row.split(","));
+
+  // Skip header row and filter out empty rows
+  return rows
+    .slice(1)
+    .map((row) => ({
+      creatorSlug: row[0].trim(),
+      projectSlug: row[1].trim(),
+    }))
+    .filter((datum) => datum.creatorSlug && datum.projectSlug)
+    .map((datum) => `${datum.creatorSlug}/${datum.projectSlug}`);
+}
+
+function shuffle<T>(array: T[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 export default function ProjectList() {
-  const slugs: string[] = [
-    "ironspike/rigsby-wi-volume-2-burn-it-down-by-se-case",
-    "ww3/poisongirls",
-    "restorationgames/return-to-dark-tower-expeditions",
-    "shiftrpg/shift-rpg",
-    "apexpublications/apex-magazine-2026",
-    "theninagalaxy/to-love-like-venus",
-    "746734715/barbarous-chapter-6-leeds-sized-edition",
-    "secondatbest/those-that-inherit-the-earth-volume-1",
-    "dwythe/danse-macabre-medieval-horror-roleplaying",
-    "brainfruitstudio/starfriends-card-game",
-  ];
+  const [slugs, setSlugs] = useState<string[] | null>(null);
+
+  useEffect(() => {
+    fetchFromSheets().then((data) => setSlugs(data));
+  }, []);
+
+  if (slugs === null) return <LoadingSpinner />;
+
+  shuffle(slugs);
 
   return (
     <>
       {slugs.map((slug) => (
-        <li class="m-1">
+        <li class="m-1" key={slug}>
           <iframe
             class="sm:w-[248px] sm:h-[397px] w-[170px] h-[360px]"
             src={`https://www.kickstarter.com/projects/${slug}/widget/card.html?v=2`}
+            scrolling="no"
           ></iframe>
         </li>
       ))}
